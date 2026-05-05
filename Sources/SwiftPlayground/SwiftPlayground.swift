84055f85d42aae70faa9419197fd4dbf67a18566
oc-elland-peleti-26 <elland.peleti@student.onslow.school.nz>
2026-05-05 20:53:16 +0000

    May 5, 2026, 8:53 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,18 +4,7 @@
 @main
 struct SwiftPlayground {
     static func main() {
-    print("Enter an adjective:")
-        let adjective1 = readLine()!
-
-        print("Enter a noun:")
-        let noun1 = readLine()!
-
-        print("Enter a verb:")
-        let verb1 = readLine()!
-
-        let madLibString = "Once upon a time, there was a \(adjective1) \(noun1) who loved to \(verb1) everyday."
-
-print(madLibString)
+    
 
 }
 }
